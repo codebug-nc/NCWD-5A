a80c36ec0d78563c6ce9f1438cf878b38b46905d
Carsen Critchell <carsencritchell@Carsens-MacBook-Air.local>
2019-07-12 14:21:23 -0400

    finished exercize

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() { 
-  // write your javascript code below.
- 
+ //   write your javascript code below.
+ 	
+ // var age = prompt ("how old are you") ;
+ // 	if (age <18) {
+ // 		alert ("Sorry you can't buy tickets")
+ // 	} else {
+ // 		alert ("I hope you win");
+ // 	}
+
+// var snackType = prompt ("What snack would you like, sweet or salty?");
+// if (snackType=="sweet"){
+// 	alert ("heres a cookie")
+
+// } else if (snackType) == ("salty"){
+
+// }else{
+// 	alert (I no get it);
+// }
+
+var favSport=prompt ("whats your favorite sport?")
+if (favSport == "basketball"){
+	alert ("thats  c00l")
+} else if (favSport== "hockey"){
+	alert ("thats cool!")
+} else if (favSport== "soccer"){
+	alert ("thats cool!")
+} 
+
+
+
+
+
+
+
+
 
 });
